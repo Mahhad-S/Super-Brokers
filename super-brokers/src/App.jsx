217ef27bb165ef7c94976f97f979eb69bda5dc17
Anh Trinh <anh.trinh.864@my.csun.edu>
2024-10-01 14:08:02 -0700

    added route for root url

--- super-brokers/src/App.jsx
+++ super-brokers/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Route path='/register' element={<Signup />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/home' element={<Home />}></Route>
+          <Route path='/' element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
